89732f813565342b28ebc2e7e2b51c3c76a40f78
fishming <1661342068@qq.com>
2023-11-11 18:21:34 -0800

    [Update] benchmark different dataset

--- benchmark/test_benchmark.py
+++ benchmark/test_benchmark.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn.functional as F
 
 import torch_geometric.typing
+import torch_geometric.datasets as datasets
 from torch_geometric.data import Data
 from torch_geometric.loader import NeighborLoader
 from torch_geometric.nn import SAGEConv
@@ -26,11 +27,37 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--backward', action='store_true')
+    parser.add_argument('--dataset', type=str, default='rand')
     args = parser.parse_args()
 
-    num_nodes, num_edges = 10_000, 200_000
-    x = torch.randn(num_nodes, 64, device=args.device)
-    edge_index = torch.randint(num_nodes, (2, num_edges), device=args.device)
+    if args.dataset == 'rand':
+        num_nodes, num_edges = 10_000, 200_000
+        x = torch.randn(num_nodes, 64, device=args.device)
+        edge_index = torch.randint(num_nodes, (2, num_edges), device=args.device)
+    else:
+        if args.dataset == 'pubmed':
+            dataset = datasets.Planetoid(root='./data/', name='Pubmed')
+            graph = dataset[0]
+        elif args.dataset == 'citeseer':
+            dataset = datasets.Planetoid(root='./data/', name='Citeseer')
+            graph = dataset[0]
+        elif args.dataset == 'cora':
+            dataset = datasets.Planetoid(root='./data/', name='Cora')
+            graph = dataset[0]
+        elif args.dataset == 'ppi':
+            dataset = datasets.PPI(root='./data/')
+            graph = dataset[0]
+        elif args.dataset == 'reddit':
+            dataset = datasets.Reddit(root='./data/Reddit')
+            graph = dataset[0]
+        elif args.dataset == 'github':
+            dataset = datasets.GitHub(root='./data/')
+            graph = dataset[0]
+        else:
+            raise KeyError('Unknown dataset {}.'.format(args.dataset))
+        num_nodes = graph.num_nodes
+        edge_index = graph.edge_index.to(args.device)
+        x = torch.randn(num_nodes, 64, device=args.device)
 
     for Model in [GCN, GraphSAGE, GIN, GAT]:
         print(f'Model: {Model.__name__}')
